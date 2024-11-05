de4743c7959dcde9f63751cb14bc8b8cf7abf334
Jeremy Wood <jeremy.wood@7factor.io>
2024-11-05 11:23:53 -0500

    Add package, linting, typescript, and vitest config.

---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+// @ts-check
+
+import eslintJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintTs from 'typescript-eslint';
+
+export default [
+  eslintJs.configs.recommended,
+  ...eslintTs.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.mjs'],
+    ...eslintTs.configs.disableTypeChecked,
+  },
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
+  {
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
+      'import/no-extraneous-dependencies': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: ['external', 'builtin', 'internal', 'sibling', 'parent', 'index'],
+          pathGroups: [],
+          pathGroupsExcludedImportTypes: ['internal'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+        },
+      ],
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: true,
+        },
+      ],
+    },
+  },
+];
